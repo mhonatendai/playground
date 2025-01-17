ca08c8d6786b29b586d31d6966e18bfef3a02213
Tendai E. Mhona <mhonatendai@gmail.com>
2025-01-16 23:45:00 -0500

    Init 1.0

---
+++ src/main/java/org/playground/KeyGenerator.java
@@ -0,0 +1,15 @@
+package org.playground;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class KeyGenerator {
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[32];
+        random.nextBytes(key);
+
+        String base64Key = Base64.getEncoder().encodeToString(key);
+        System.out.println(base64Key);
+    }
+}
